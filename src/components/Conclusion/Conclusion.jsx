5189771db13ce2ef7f864186dbc3f7524fdf9848
knox <kknoxy@yahoo.com>
2025-10-24 11:53:52 +0100

    chhanged y scroll view point in conclusion component and added load dash

--- src/components/Conclusion/Conclusion.jsx
+++ src/components/Conclusion/Conclusion.jsx
@@ -11,7 +11,7 @@ const Conclusion = () => {
       <div className='container'>
         <motion.div
           className='content-container'
-          initial={{ opacity: 0, y: 100 }}
+          initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{
             duration: 0.5,
